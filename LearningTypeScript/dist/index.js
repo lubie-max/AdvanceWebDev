69760128f94735feccae9f6deda0efc53211f84c
Lubna Shaikh <Lubna.Shaikh@learningmatemumbai.local>
2024-04-19 20:26:31 +0530

    objects

--- LearningTypeScript/dist/index.js
+++ LearningTypeScript/dist/index.js
@@ -16,14 +16,31 @@
 // let arr2: number[] = [1,'3']; // TC syntax throws exception
 // 2. Tuples
 let User = ['Lubna', 22]; //if we  pass more than 2 arguments then it will throw exception.
-;
-let firstPrice = 12 /* Prices.Second */; //
-console.log(firstPrice);
+// 3. Enum  : it's basically a function that provides a count 
+var Prices;
+(function (Prices) {
+    Prices[Prices["First"] = 11] = "First";
+    Prices[Prices["Second"] = 12] = "Second";
+})(Prices || (Prices = {}));
+; //  value assigned to the First is starting point of counter/ index
+let firstPrice = Prices.Second; //
+// console.log(firstPrice)
 // 4. Functions
-function UserData(name) {
+// age?:number : ? means age is optional here. while compilation if we don't pass age, it will not goin to throw an exception
+function UserData(name, age) {
     // we have to enable few functionalities here from tsconfig.json like "noImplicitReturns": true,  for not implecit returs
     let x = name.split('#');
     return x;
 }
 // console.log(UserData('Lubna,shai#kh'))
+//5. Objects 
+let Employee = {
+    id: 1,
+    name: 'Lubna',
+    retirementDATE: (date) => {
+        console.log(date);
+    }
+};
+Employee.name = "shaikh";
+console.log(Employee.name, Employee.retirementDATE);
 //# sourceMappingURL=index.js.map
--- LearningTypeScript/src/index.ts
+++ LearningTypeScript/src/index.ts
@@ -35,11 +35,11 @@ let User: [string, number] = ['Lubna', 22] //if we  pass more than 2 arguments t
 enum Prices {First =11, Second}; //  value assigned to the First is starting point of counter/ index
 let firstPrice: Prices = Prices.Second; //
 
-console.log(firstPrice)
+// console.log(firstPrice)
 
 // 4. Functions
-
-function UserData(name: string): any{
+// age?:number : ? means age is optional here. while compilation if we don't pass age, it will not goin to throw an exception
+function UserData(name: string , age?:number): any{
     // we have to enable few functionalities here from tsconfig.json like "noImplicitReturns": true,  for not implecit returs
 
     let x = name.split('#')
@@ -49,5 +49,24 @@ function UserData(name: string): any{
 
 // console.log(UserData('Lubna,shai#kh'))
 
+
+
+//5. Objects 
+
+let Employee:{
+     readonly id: number,  // readonly tsc feature which does not let the property be modified.
+     name: string,
+     retirementDATE: (date:Date)=> void,  // expects a date function 
+} = {
+    id : 1,
+    name : 'Lubna',
+    retirementDATE: (date:Date)=> {
+        console.log(date)
+    }
+
+
+}
+Employee.name= "shaikh"
+console.log(Employee.name, Employee.retirementDATE)
 
 
